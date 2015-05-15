d9d54ee7623dbc21d0dac931e19eb11423625236
codingeass <amandeeptheviper@gmail.com>
2015-05-15 21:12:37 +0530

    comment tag addition removed in addcontent

--- public/admin/php/addcontent.php
+++ public/admin/php/addcontent.php
@@ -1,13 +1,12 @@
 <?php
 	require("sessionv.php");
 	if(isset($_REQUEST["title"])&&isset($_REQUEST["content"])){
-		$xml=simplexml_load_file("../../xml/chatting.xml");
+		$xml=simplexml_load_file("../../xml/blog.xml");
 		$blogsection=$xml->addChild('BlogSection');
 		$blogsection->addChild('title',$_REQUEST["title"]);
 		$blogsection->addChild('author',$_SESSION["user"]);
 		$blogsection->addChild('content',$_REQUEST["content"]);
-		$blogsection->addChild('comments');
-		$xml->saveXML('../../xml/chatting.xml');
+		$xml->saveXML('../../xml/blog.xml');
 		echo "Correct";
 	}
 	else
