32627710b74887665523b362be4e8bb11f490f18
codingeass <amandeeptheviper@gmail.com>
2015-05-23 15:31:47 +0530

    time updated

--- public/admin/php/upcontent.php
+++ public/admin/php/upcontent.php
@@ -3,6 +3,7 @@
 	if(isset($_REQUEST["title"])&&isset($_REQUEST["content"])&&isset($_REQUEST["i"])){
 		$xml=simplexml_load_file("../../xml/blog.xml");
 		$blogsection=$xml->BlogSection[intval($_REQUEST["i"])];
+		$blogsection->addChild('time',date("F j, Y, g:i a"));
 		$blogsection->title=$_REQUEST["title"];
 		$blogsection->content=$_REQUEST["content"];
 		$xml->saveXML('../../xml/blog.xml');
