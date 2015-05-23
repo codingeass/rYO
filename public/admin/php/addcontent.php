be72660cc522e2de5b8c494a59c8fa3b22125a04
codingeass <amandeeptheviper@gmail.com>
2015-05-23 15:29:03 +0530

    updated new blog addition and added htmlspecial char check

--- public/admin/php/addcontent.php
+++ public/admin/php/addcontent.php
@@ -3,9 +3,14 @@
 	if(isset($_REQUEST["title"])&&isset($_REQUEST["content"])){
 		$xml=simplexml_load_file("../../xml/blog.xml");
 		$blogsection=$xml->addChild('BlogSection');
-		$blogsection->addChild('title',$_REQUEST["title"]);
-		$blogsection->addChild('author',$_SESSION["user"]);
-		$blogsection->addChild('content',$_REQUEST["content"]);
+		$xml->max_id[0]=intval($xml->max_id[0])+1;
+		$xml->max_sequence[0]=intval($xml->max_sequence[0])+1;
+		$blogsection->addChild('id',$xml->max_id[0]);
+		$blogsection->addChild('sequence',$xml->max_sequence[0]);
+		$blogsection->addChild('time',date("F j, Y, g:i a"));
+		$blogsection->addChild('title',htmlspecialchars(urldecode(strip_tags($_REQUEST["title"])), ENT_QUOTES));
+		$blogsection->addChild('author',htmlspecialchars(urldecode(strip_tags($_SESSION["user"])), ENT_QUOTES));
+		$blogsection->addChild('content',htmlspecialchars(urldecode(strip_tags($_REQUEST["content"])), ENT_QUOTES));
 		$xml->saveXML('../../xml/blog.xml');
 		echo "Correct";
 	}
