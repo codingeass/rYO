d78cf518af8702ab3f41424595028bf1e720d0e0
codingeass <amandeeptheviper@gmail.com>
2015-06-04 18:43:43 +0530

    checking for social card

--- public/js/main.js
+++ public/js/main.js
@@ -23,3 +23,13 @@ for (i=0;i<x.length;i++)
 	mk=mk+"<div id='s_author'>Author:"+x[i].getElementsByTagName("author")[0].childNodes[0].nodeValue+"</div></section>"
 }
 mg.innerHTML=mk;
+
+
+function meta_social_tag(title,image,url,description){
+	var meta='<meta property="og:title" content="'+title+'" /><meta property="og:type" content="article" /> \
+        <meta property="og:image" content="'+image+'" /> \
+        <meta property="og:url" content="'+url+'" /> \
+        <meta property="og:description" content="'+description+'" />';
+	document.getElementsByTagName('head')[0].innerHTML=document.getElementsByTagName('head')[0].innerHTML+meta; 
+}
+meta_social_tag("Amandeep Gupta","http://codingeass.github.io/dist/img/profile.png","http://codingeass.github.io","Portfolio ,Web Developer | Software Developer | Programmer");
